84623b204e8b94ef2fc5698cd02fd26c8a0f0914
amahammadli20 <aytajmahammadl@gmail.com>
2025-05-08 18:31:02 -0700

    Hydra setup

---
+++ image_caption/run_config.py
@@ -0,0 +1,12 @@
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(config_path="../config", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    print("Model type:", cfg.model.type)
+    print("Embedding dim:", cfg.model.lstm.embedding_dim)
+    print("Optimizer:", cfg.optimizer.type)
+    print("Learning rate:", cfg.optimizer.learning_rate)
+
+if __name__ == "__main__":
+    main()
